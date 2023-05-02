c3dcf29c3f6c0267d37bc65195cdb3def251e22c
Jim Lanham <jrl5748@psu.edu>
2023-05-02 15:48:57 -0400

    Generate workout dashboard componenet.

--- src/app/feature/workout/workout.module.ts
+++ src/app/feature/workout/workout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WorkoutRoutingModule } from './workout-routing.module';
-import { WorkoutComponent } from './workout.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { CreateWorkoutComponent } from './component/create-workout/create-workout.component';
 
 
 @NgModule({
   declarations: [
-    WorkoutComponent
+    DashboardComponent,
+    CreateWorkoutComponent
   ],
   imports: [
     CommonModule,
