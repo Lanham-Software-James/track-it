1509ffc6d2ff96898e3b5c92394d269ad01956eb
Jim Lanham <jrl5748@psu.edu>
2023-05-02 11:04:05 -0400

    Minimum code coverage is 95%

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95
+    }
+  }
 };
