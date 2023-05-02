26d1a9ce48b68668bb984533c3682b8e1db46811
Jim Lanham <jrl5748@psu.edu>
2023-05-02 15:56:29 -0400

    Cleaning up routing files.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./feature/dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'workout', loadChildren: () => import('./feature/workout/workout.module').then(m => m.WorkoutModule) }];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./feature/dashboard/dashboard.module').then(m => m.DashboardModule) }, 
+  { path: 'workout', loadChildren: () => import('./feature/workout/workout.module').then(m => m.WorkoutModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/feature/workout/workout-routing.module.ts
+++ src/app/feature/workout/workout-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WorkoutComponent } from './workout.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
-const routes: Routes = [{ path: '', component: WorkoutComponent }];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'create', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
